e42824e7a5c9f8758d49d85236e1bf8dbbf2b75c
mahmoudadel810 <ma.adel.810@gmail.com>
2025-07-25 19:29:36 +0300

    more debug

--- src/pages/PurchaseSuccessPage.jsx
+++ src/pages/PurchaseSuccessPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
